1e6f3a432691bc89056a7a88ac68077957e76576
arya satwika <aryastwk95@gmail.com>
2025-06-04 12:01:49 +0700

    filter tutor dan isi data

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-print("Hello, World!")
-print("This is a Python script.")
-print("It prints messages to the console.")
-print("asodjasodadoakd")
-print("This is a test message.")
-print("This is another test message.")  
-print("Migguel Suka Harsya.") #testing
-print("Push karo pull beda ta")
-print("iya, push itu upload ke repo, pull itu ngambil dr repo")
+import json
+
+users = json.load(open("data.json"))
+
+class User:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.loadUsers()
+    def loadAllUsers(self):
+        user_list = users.get("users", [])
+        return user_list
+    def filterByMatkul(self, matkul):
+        filtered_list = [
+            filtered_user 
+            for filtered_user in users.get("users", [])
+                if matkul in filtered_user.get("matkul", [])]
+        return filtered_list
+class matkul:
+    def __init__(self, name, code):
+        self.name = name
+        self.code = code
+

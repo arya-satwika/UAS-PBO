33e6b7b7bfac7b13dfc12a01cdc88dca6d665da6
arya satwika <aryastwk95@gmail.com>
2025-06-04 12:07:55 +0700

    nambah checklist & ganti key di json

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ class User:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.loadUsers()
-    def loadAllUsers(self):
-        user_list = users.get("users", [])
+        self.loadAllTutors()
+    def loadAllTutors(self):
+        user_list = users.get("tutor", [])
         return user_list
     def filterByMatkul(self, matkul):
         filtered_list = [
-            filtered_user 
-            for filtered_user in users.get("users", [])
+            filtered_user
+            for filtered_user in users.get("tutor", [])
                 if matkul in filtered_user.get("matkul", [])]
         return filtered_list
 class matkul:
